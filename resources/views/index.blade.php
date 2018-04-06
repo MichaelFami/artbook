91b087f912ddb4a126cb7a8987e7637b50bad0d3
Unknown <vmrdz220@gmail.com>
2018-04-06 14:36:23 -0500

    removed some testimonials

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -165,7 +165,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="carousel-item">
+                <!-- <div class="carousel-item">
                     <div class="container mb-5">
                         <div class="row">
                             <div class="col-lg-6 mt-2">
@@ -187,6 +187,28 @@
                         </div>
                     </div>
                 </div>
+                <div class="carousel-item">
+                    <div class="container mb-5">
+                        <div class="row">
+                            <div class="col-lg-6 mt-2">
+                                <p class=""><i class="fas fa-quote-right fa-2x"></i></p>
+                                <div class="d-flex flex-row">
+                                    <div class="align-self-start">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 mt-2">
+                                <p class=""><i class="fas fa-quote-right fa-2x"></i></p>
+                                <div class="d-flex flex-row">
+                                    <div class="align-self-start">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
             </div>
             <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
